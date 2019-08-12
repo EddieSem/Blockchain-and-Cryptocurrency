4b592a95a6ba543048be79389a1ff1d8cd81b833
Edward Semugabi <ESemugabi@idbs.com>
2019-08-12 11:23:51 +0100

    [Extract and Append in different functions 1:1 ratio]
    Committer: Eddie Semugabi <essno.1@hotmail.co.uk>

--- blockchain.py
+++ blockchain.py
@@ -1,15 +1,18 @@
-blockchain = [[1]]
+blockchain = []
 
 # Extract out last blockchain value and return it.
 def get_last_blockchain_value():
     return blockchain[-1]
 
-# Create the blockchain in one function. 
-def add_value(transaction_amount):
-    blockchain.append([get_last_blockchain_value(), transaction_amount])
-    
+
+# Append to blockchain in the right order, default first. 
+def add_value(transaction_amount, last_transaction=[1]):
+    blockchain.append([last_transaction, transaction_amount])
+
+# Append to newly created initial block of the blockchain   
 add_value(8)
-add_value(0.87)
-add_value(1010019874764.35365)
+# Return preivous block of the blockchain and append.
+add_value(0.87, get_last_blockchain_value())
+add_value(1010019874764.35365, get_last_blockchain_value())
 
 print(blockchain)
